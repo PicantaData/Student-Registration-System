48ec345cd6a2156cf075fa7463c372a1d5e74dbd
Abhinav2311 <aabhinav536@gmail.com>
2023-11-25 14:41:15 +0530

    Added unit testing files

---
+++ SRS/main/tests/test_models.py
@@ -0,0 +1,178 @@
+from django.test import TestCase
+from main import models
+from django.contrib.auth.models import User
+from datetime import date, datetime
+
+class Test_ApplicationModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='Test@1234')
+        self.application = models.Application.objects.create(
+            student=self.user,
+            name='Test Applicant',
+            gender='M',
+            dob=date(2000, 1, 1),
+            father='Test Person1',
+            mother='Test Person2',
+            phone='9988776655',
+            alt_phone='9876543210',
+            address='Test Address',
+            app_status='P',
+            ssc='School1',
+            ssc_per='90',
+            hsc='School2',
+            hsc_per='85',
+            gujcet='GujCET Details',
+            jee='JEE Details',
+            id_proof='Aadhar.pdf',
+            photo = 'photo.jpg',
+            marks_10='test_10.pdf', 
+            marks_12='test_12.pdf',
+        )
+
+    def test_applicantion_creation(self):
+        self.assertEqual(self.application.name, 'Test Applicant')
+        self.assertEqual(models.Application.objects.count(), 1) 
+
+    def test_delete_applicant(self):
+        self.application.delete()
+        self.assertFalse(models.Application.objects.filter(name='Test Applicant').exists())
+
+
+class Test_NotoficationModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='Test@1234')
+        self.application = models.Application.objects.create(
+            student=self.user,
+            name='Test Applicant',
+            gender='M',
+            dob=date(2000, 1, 1),
+            father='Test Person1',
+            mother='Test Person2',
+            phone='9988776655',
+            alt_phone='9876543210',
+            address='Test Address',
+            app_status='P',
+            ssc='School1',
+            ssc_per='90',
+            hsc='School2',
+            hsc_per='85',
+            gujcet='GujCET Details',
+            jee='JEE Details',
+            id_proof='Aadhar.pdf',
+            photo = 'photo.jpg',
+            marks_10='test_10.pdf', 
+            marks_12='test_12.pdf',
+        )
+        self.notification = models.Notification.objects.create(
+            filter_flag='S',
+            recipient=self.application,
+            content='Test Notification'
+        )
+
+    def test_notification_creation(self):
+        self.assertEqual(models.Notification.objects.count(),1)
+        self.assertEqual(self.notification.filter_flag, 'S')
+        self.assertEqual(self.notification.recipient.name, 'Test Applicant')
+
+     
+class Test_QuestionModel(TestCase):
+    def setUp(self):
+        self.question = models.Question.objects.create(
+            qid=1,
+            ques='Test Question',
+            op1='Option 1',
+            op2='Option 2',
+            op3='Option 3',
+            op4='Option 4',
+            ans='1'
+        )
+
+    def test_question_creation(self):
+        self.assertEqual(models.Question.objects.count(),1)
+        self.assertTrue(models.Question.objects.filter(qid=1).exists())
+
+
+
+class Test_ApplicantResponseModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='Test@1234')
+        self.application = models.Application.objects.create(
+            student=self.user,
+            name='Test Applicant',
+            gender='M',
+            dob=date(2000, 1, 1),
+            father='Test Person1',
+            mother='Test Person2',
+            phone='9988776655',
+            alt_phone='9876543210',
+            address='Test Address',
+            app_status='P',
+            ssc='School1',
+            ssc_per='90',
+            hsc='School2',
+            hsc_per='85',
+            gujcet='GujCET Details',
+            jee='JEE Details',
+            id_proof='Aadhar.pdf',
+            photo = 'photo.jpg',
+            marks_10='test_10.pdf', 
+            marks_12='test_12.pdf',
+        )
+        self.question = models.Question.objects.create(
+            qid=3,
+            ques='Test Question3',
+            op1='Option 1',
+            op2='Option 2',
+            op3='Option 3',
+            op4='Option 4',
+            ans='1'
+        )
+
+    def test_response_creation(self):
+        self.applicant_response = models.ApplicantResponse.objects.create(
+            app_no=self.application,
+            ques=self.question,
+            response='1'
+        )
+        self.assertEqual(models.ApplicantResponse.objects.count(), 1)
+        self.assertEqual(self.applicant_response.response,'1')
+
+
+class Test_TestModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='Test@1234')
+        self.application = models.Application.objects.create(
+            student=self.user,
+            name='Test Applicant',
+            gender='M',
+            dob=date(2000, 1, 1),
+            father='Test Person1',
+            mother='Test Person2',
+            phone='9988776655',
+            alt_phone='9876543210',
+            address='Test Address',
+            app_status='P',
+            ssc='School1',
+            ssc_per='90',
+            hsc='School2',
+            hsc_per='85',
+            gujcet='GujCET Details',
+            jee='JEE Details',
+            id_proof='Aadhar.pdf',
+            photo = 'photo.jpg',
+            marks_10='test_10.pdf', 
+            marks_12='test_12.pdf',
+        )
+
+    def test_test_creation(self):
+        test = models.Test.objects.create(
+            app_no=self.application,
+            score=30,
+        )
+
+        self.assertEqual(models.Test.objects.count(), 1)
+        self.assertEqual(test.score,30)
+
+
+        
+
